43f4d66c8bfc04572a79d680b21f25ea109c5923
Anh Phan <anhphanvt2705@gmail.com>
2024-07-11 17:00:55 +0700

    specified data source

--- tools/data_preprocessing.py
+++ tools/data_preprocessing.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 from utils import get_combined_dataset, convert_to_dataframe, save_to_csv
 
-def preprocess_data(df: pd.DataFrame, target_vars: str) -> pd.DataFrame:    
+def preprocess_cds_data(cds_df: pd.DataFrame, target_vars: str) -> pd.DataFrame:    
     return None
 
-def main(data_paths: list, target_vars: list, save_dir: str = ''):
+def main(data_paths: list, target_vars: list, data_source: str = 'cds', save_dir: str = ''):
     # Load the data
     ds = get_combined_dataset(data_paths)
     
@@ -14,7 +14,8 @@ def main(data_paths: list, target_vars: list, save_dir: str = ''):
     df = convert_to_dataframe(ds, variables=target_vars)
 
     # Preprocess the data
-    preprocessed_data = preprocess_data(df, target_vars)
+    if data_source == 'cds':
+        preprocessed_data = preprocess_cds_data(df, target_vars)
     
     # Save the preprocessed data
     if save_dir:
